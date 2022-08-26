666909c1399afa9e426acf083689915ac0ae5a93
NehaPardeshi2403 <pardeshineha0399@gmail.com>
2022-08-26 23:02:42 +0530

    Updated main file, changed sort selection logic

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import com.code.sorts.bubbleSort as bubbleSort
 import com.code.sorts.selectionSort as selectionSort
 import com.code.sorts.insertionSort as insertionSort
-#import com.code.sorts.quickSort as quickSort
-A = [3, 4, 9, 10, 3, 2, 120]
-bubble = "bubbleSort"
-selection = "selectionSort"
-insertion = "insertionSort"
-userInput = input("Enter the sorting method: ")
 
-if bubble == userInput:
+# import com.code.sorts.quickSort as quickSort
+A = [3, 4, 9, 10, 3, 2, 120]
+
+userInput = int(input("Enter the sorting method: \n"
+                      "For Bubble Sort Enter : 1 \n"
+                      "For Selection Sort Enter : 2 \n"
+                      "For Insertion Sort Enter : 3 \n"
+                      "For Nothing  Enter : 4  : "))
+
+if userInput == 1:
     print("Using Bubble Sort")
     bubbleSort.bubble_sort(A)
-elif selection == userInput:
+elif userInput == 2:
     print("Using Selection  Sort")
     selectionSort.selection_sort(A)
-elif insertion == userInput:
+elif userInput == 3:
     print("Using Insertion Sort")
     insertionSort.insertion_sort(A)
 else:
     print("there is no another sorting method")
-
